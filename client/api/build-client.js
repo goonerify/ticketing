58a965db7211e2197be31b9e5b4badb1afda4420
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-22 21:54:01 -0400

    Try catch

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,16 +4,26 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
 
-    // Create a pre-configured version of axios client with a baseurl, domain, headers wired up
-    return axios.create({
-      baseURL: "http://40.88.54.78",
-      headers: req.headers,
-    });
+    try {
+      // Create a pre-configured version of axios client with a baseurl, domain, headers wired up
+      return axios.create({
+        baseURL: "http://40.88.54.78",
+        headers: req.headers,
+      });
+    } catch (e) {
+      console.log("Error 1");
+      console.dir(err);
+    }
   } else {
     // We must be on the
     // Create a pre-configured version of axios client
-    return axios.create({
-      baseUrl: "/",
-    });
+    try {
+      return axios.create({
+        baseUrl: "/",
+      });
+    } catch (e) {
+      console.log("Error 2");
+      console.dir(err);
+    }
   }
 };
